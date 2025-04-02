a4acd63d29bdb1fe17e423ded3e41625d4091cc1
Alinani <alinanisilomba1@gmail.com>
2025-04-02 17:48:00 +0200

    Add comments to serializers and models for better code documentation

--- BookApp/api/Serializers.py
+++ BookApp/api/Serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Book
 
-
+# This serializer is used to serialize the data from the Book model.
+# It is used to convert the data from the model to JSON format and vice versa.
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
--- BookApp/api/models.py
+++ BookApp/api/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-
+# This is the model that we will use to create the database table.
+# # This model is used to create the database table for the books.
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     description = models.TextField()
    
     def __str__(self):
-        return self.title
+        return f'{self.title} by {self.author}'
+    # This method is used to return the string representation of the model.
